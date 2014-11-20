86a87df2438f65a68046cd05bec003fa6531b87d
deploy <deploy@ryantest1.(none)>
2014-11-20 16:00:44 -0500

    adding a bunch of stuff

--- gimmelink_cli.js
+++ gimmelink_cli.js
@@ -3,6 +3,33 @@ var aws = require('aws-sdk');
 var Chance = require('chance'),
     chance = new Chance();
 
-var chance_string = chance.string({length: 7, pool: 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'});
+// AWS Credential Config
+aws.config.loadFromPath('./config.json');
 
-var echo_url = console.log('http://' + chance_string + '.gimme.link');
+var route53 = new aws.route53(); // instantiate Route53 API client
+
+var chance_string = chance.string({length: 7, pool: 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'}); // Random String Generator for CNAMEs
+
+//var echo_url = console.log('http://' + chance_string + '.gimme.link');
+
+var params = {
+	ChangeBatch: {
+		Changes: [
+			{
+				Action: 'CREATE',
+				ResourceRecordSet: {
+					Name: chance_string,
+					Type: 'CNAME',
+					TTL: 300
+				}
+		],
+		}
+	}
+	HostedZoneId: 'ZONEGOESHERE'
+};
+
+
+route53.changeResourceRecordSets(params, function(err, data) {
+	if (err) console.log(err, err.stack); // Something gwaan get mashup.
+	else		console.log(data);			  // Yay it worked!!
+}
